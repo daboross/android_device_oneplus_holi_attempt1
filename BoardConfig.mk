addde38bbdb2e842fbcaf2fd6af3ee6c26d38147
David Ross <daboross@daboross.net>
2021-08-02 02:35:27 -0700

    Let's see if this works. :(

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 vi
 BOARD_KERNEL_CMDLINE += skip_override androidboot.fastboot=1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
-# TARGET_PREBUILT_KERNEL := device/oneplus/holi/prebuilt/Image.gz-dtb
+TARGET_PREBUILT_KERNEL := device/oneplus/holi/prebuilt/Image.gz-dtb
 
 # Platform
 TARGET_BOARD_PLATFORM := sm4350
